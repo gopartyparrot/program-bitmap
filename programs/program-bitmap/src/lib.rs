8e5935504ff523be1c4649fdacb97d7356e816ea
sssrot <sunss@tutanota.com>
2021-09-18 09:22:57 +0800

    program

---
+++ programs/program-bitmap/src/lib.rs
@@ -0,0 +1,126 @@
+use anchor_lang::prelude::*;
+use bitmap::Bitmap;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+mod bitmap;
+
+#[program]
+pub mod program_bitmap {
+    use crate::bitmap::Bitmap;
+
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let len: usize = ctx.accounts.ob.to_account_info().data_len();
+        let ob = &mut ctx.accounts.ob;
+        ob.owner = *ctx.accounts.owner.to_account_info().key;
+        ob.bitmap = Vec::<u8>::bm_with_capacity((len - 8 - 32) * 8); //exclude sig hash and owner space
+        Ok(())
+    }
+
+    // current value in $index must be !$value then set new value to $value
+    pub fn must_swap(ctx: Context<Admin>, iv: IndexValue) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        ob.check_index(iv.index)?;
+        if ob.bitmap.bm_get(iv.index) != !iv.value {
+            return Err(ErrorCode::ValueNotMatch.into());
+        }
+        ob.bitmap.bm_set(iv.index, iv.value);
+        Ok(())
+    }
+
+    pub fn must_swap_batch(ctx: Context<Admin>, ivs: Vec<IndexValue>) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        for iv in ivs {
+            ob.check_index(iv.index)?;
+            if ob.bitmap.bm_get(iv.index) != iv.value {
+                return Err(ErrorCode::ValueNotMatch.into());
+            }
+            ob.bitmap.bm_set(iv.index, !iv.value);
+        }
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Admin>, iv: IndexValue) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        ob.check_index(iv.index)?;
+        ob.bitmap.bm_set(iv.index, iv.value);
+        Ok(())
+    }
+
+    pub fn set_batch(ctx: Context<Admin>, ivs: Vec<IndexValue>) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        for iv in ivs {
+            ob.check_index(iv.index)?;
+            ob.bitmap.bm_set(iv.index, iv.value);
+        }
+        Ok(())
+    }
+
+    pub fn reset(ctx: Context<Admin>) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        ob.bitmap.bm_reset();
+        Ok(())
+    }
+
+    pub fn set_owner(ctx: Context<SetOwner>) -> Result<()> {
+        let ob = &mut ctx.accounts.ob;
+        ob.owner = *ctx.accounts.new_owner.to_account_info().key;
+        Ok(())
+    }
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize)]
+pub struct IndexValue {
+    pub index: u32,
+    pub value: bool,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(zero)]
+    pub ob: Account<'info, OwnedBitmap>,
+    #[account(signer)]
+    pub owner: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Admin<'info> {
+    #[account(has_one = owner)]
+    pub ob: Account<'info, OwnedBitmap>,
+    #[account(signer)]
+    pub owner: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SetOwner<'info> {
+    #[account(has_one = owner)]
+    pub ob: Account<'info, OwnedBitmap>,
+    #[account(signer)]
+    pub owner: AccountInfo<'info>,
+    #[account()]
+    pub new_owner: AccountInfo<'info>,
+}
+
+#[account]
+pub struct OwnedBitmap {
+    owner: Pubkey,
+    bitmap: Vec<u8>,
+}
+
+impl OwnedBitmap {
+    pub fn check_index(&self, index: u32) -> Result<()> {
+        if self.bitmap.bm_capacity() <= index {
+            return Err(ErrorCode::IndexOverflow.into());
+        }
+        Ok(())
+    }
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("value not match")]
+    ValueNotMatch,
+    #[msg("index overflow")]
+    IndexOverflow,
+}
